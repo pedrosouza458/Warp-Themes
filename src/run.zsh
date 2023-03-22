cc7321eb632ea7d45d2623b0397ef94fc470d6fb
Pedro Souza <pedrosouza.ch453@academico.ifsul.edu.br>
2023-03-22 19:38:05 -0300

    adding zsh

---
+++ src/run.zsh
@@ -0,0 +1,5 @@
+# add the themes by running this lines in your terminal
+
+curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/pedrosouza458/warp-themes/main/themes/warp_hoppscotch.yaml
+curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/pedrosouza458/warp-themes/main/themes/warp_omni.yaml
+curl --output-dir ~/.warp/themes -LO https://raw.githubusercontent.com/pedrosouza458/warp-themes/main/themes/warp_rose_pine.yaml
